11b13caa1a659b7252165356667b44d669deed8c
chux2000 <chux@IMAGINA.localdomain>
2021-08-01 17:39:06 -0400

    update installer

--- install.sh
+++ install.sh
@@ -7,9 +7,9 @@ cd Examen_tecnico_3
 #chmod a+x -R /docker-root/ && chmod 777 -R docker-root/
 #ls -la docker-root/sbin/
 echo "**** ARMANDO CONTENEDOR   ****"
-docker build -t heroku .
+docker build -t clayvnc .
 echo "**** ACTIVANDO CONTENEDOR ****"
-docker run -it -d -p 9000:9000 -p 9001:9001 -p 5901:5901 -e VNC_PASSWD=chux2000  -e USER_PASSWD=chux2000 heroku
+docker run -it -d -p 80:9000 -p 81:9001 -p 82:5901 -e VNC_PASSWD=chux2000  -e USER_PASSWD=chux2000 clayvnc
 clear
 sleep 5
 ls -la docker-root/sbin/
