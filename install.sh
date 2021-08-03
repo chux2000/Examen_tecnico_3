cb1842d18043c43164c454ea6e2e8c7f3c26b489
chux2000 <chux@IMAGINA.localdomain>
2021-08-03 18:16:41 -0400

    update dockerfile

--- install.sh
+++ install.sh
@@ -7,10 +7,10 @@ cd Examen_tecnico_3
 #chmod a+x -R /docker-root/ && chmod 777 -R docker-root/
 #ls -la docker-root/sbin/
 echo "**** ARMANDO CONTENEDOR   ****"
-docker build -t clayvnc .
-echo "**** ACTIVANDO CONTENEDOR ****"
-docker run -it -d -p 80:9000 -p 81:9001 -p 82:5901 -e VNC_PASSWD=chux2000  -e USER_PASSWD=chux2000 clayvnc
-clear
-sleep 5
-ls -la docker-root/sbin/
-docker ps
+docker build -t chux2000/clayvnc .
+# echo "**** ACTIVANDO CONTENEDOR ****"
+#docker run -it -d -p 80:9000 -p 81:9001 -p 82:5901 -e VNC_PASSWD=chux2000  -e USER_PASSWD=chux2000 clayvnc
+#clear
+#sleep 5
+#ls -la docker-root/sbin/
+#docker ps
